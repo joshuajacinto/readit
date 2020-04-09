045aede429020d2e377cbaf96ea5e9c1827096d3
joshuajacinto <joshua.jacinto@sitepoint.com>
2020-04-09 10:04:09 +0800

    app project

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  
+  resources :stories do
+  	resources :votes do
+  end	
+ 	get 'votes/create'
+  	get 'stories/index'
+  	get 'stories/new'	
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
+end
