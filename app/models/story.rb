7fe87cd691d779280c2dcc3aeced12c5caec1f9d
joshuajacinto <joshua.jacinto@sitepoint.com>
2020-04-09 17:05:31 +0800

    updated

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,9 +1,8 @@
 class Story < ApplicationRecord
-	validates :name, :link, presence: true
-	has_many :votes
-
-	def to_param
-     "#{id}-#{name.gsub(/\W/, '-').downcase}" 
-    end  
-
+  validates :name, :link, presence: true
+  has_many :votes do
+    def latest
+      order('id DESC').limit(3)
+    end
+  end
 end
